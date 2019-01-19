95cde4183e59c3496545145b9c27e8f6f87ce41c
Ezra Fielding <ezra.fielding@gmail.com>
2019-01-19 14:41:53 +0200

    Added 'setup.py'

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='easytello',
+    version='0.0.1',
+    author='Ezra Fielding',
+    author_email='ezra.fielding@gmail.com',
+    description='An easy framework to support DJI Tello scripting in Python 3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Virodroid/easyTello',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
