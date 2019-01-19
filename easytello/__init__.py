bd0daf5a252a7c98b7373a6a3309cf568028ebf9
Ezra Fielding <ezra.fielding@gmail.com>
2019-01-19 15:37:09 +0200

    Imported submodules in '__init__.py'

--- easytello/__init__.py
+++ easytello/__init__.py
@@ -1,1 +1,9 @@
-name = "easytello"
+__title__ = 'easytello'
+__author__ = 'Ezra Fielding'
+__liscence__ = 'MIT'
+__copyright__ = 'Copyright 2019 Ezra Fielding'
+__version__ = '0.0.2'
+__all__ = ['tello', 'stats']
+
+from .tello import Tello
+from .stats import Stats
