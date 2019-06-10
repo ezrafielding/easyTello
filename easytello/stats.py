ad458bc449dd36e87d5db39db2b4ad7dfb6f994b
Ezra Fielding <ezra.fielding@gmail.com>
2019-06-10 23:44:35 +0200

    Better responses for Read Commands

--- easytello/stats.py
+++ easytello/stats.py
@@ -11,7 +11,7 @@ class Stats:
         self.duration = None
 
     def add_response(self, response: str):
-        self.response = response
+        self.response = str(response)
         # Calculating total time taken to execute command
         self.end_time = datetime.now()
         self.duration = (self.end_time-self.start_time).total_seconds()
@@ -22,6 +22,44 @@ class Stats:
         else:
             return True
 
-    def get_response(self):
+    def get_raw_response(self):
         return self.response
 
+    def numeric_response(self, data: str):
+        num_val = ''.join(i for i in data if i.isdigit() or i=='-' or i=='.')
+        return num_val
+
+    def int_response(self, data: str):
+        return int(self.numeric_response(data))
+
+    def float_response(self, data: str):
+        return float(self.numeric_response(data))
+    
+    def attitude_response(self):
+        raw_att = self.response.split(';')
+        att_data = (self.fint_response(raw_att[0]), self.int_response(raw_att[1]), self.int_response(raw_att[2]))
+        return att_data
+    
+    def acceleration_response(self):
+        raw_acc = self.response.split(';')
+        acc_data = (self.float_response(raw_acc[0]), self.float_response(raw_acc[1]), self.float_response(raw_acc[2]))
+        return acc_data
+
+    def temp_response(self):
+        raw_temp = self.response.split('~')
+        temp = (self.int_response(raw_temp[0]) + self.int_response(raw_temp[1]))/2
+        return temp
+
+    def get_response(self):
+        if 'attitude?' in self.command:
+            return self.attitude_response()
+        elif 'acceleration?' in self.command:
+            return self.acceleration_response()
+        elif 'temp?' in self.command:
+            return self.temp_response()
+        elif 'baro?' in self.command or 'speed?' in self.command:
+            return self.float_response(self.response)
+        elif '?' not in self.command:
+            return self.get_raw_response()
+        else:
+            return self.int_response(self.response)
