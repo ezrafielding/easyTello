d715d61d88a8c692db6d8894bf13b81ff3fb16ae
Ezra Fielding <ezra.fielding@gmail.com>
2020-08-20 16:54:10 +0200

    Version Change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='easytello',
-    version='0.0.7',
+    version='0.0.8',
     author='Ezra Fielding',
     author_email='ezra.fielding@gmail.com',
     description='An easy framework to support DJI Tello scripting in Python 3',
